a645f6f27f36c0b6f534c78bdfac70d088b8214a
Thinura-Nethz <thinuranethsara999@gmail.com>
2025-06-15 20:27:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "6ZUy1D5R#rxVn7v9QpCCk0e0WzItxoSIVCgf-4kXdOIRtu-8Iass",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Thinura-Nethz/AngalX-Helper/refs/heads/main/ChatGPT%20Image%20Jun%2015%2C%202025%2C%2004_55_04%20PM.png?token=GHSAT0AAAAAADEHELJS7IVTOVR5WUNBVU2C2COVYQQ",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Thinura-Nethz/HELP/refs/heads/main/ChatGPT%20Image%20Jun%2015%2C%202025%2C%2004_55_04%20PM.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hey, I'm Angal-X Multi-Deveice (MD) Whatsapp Bot.. Now I'm Alive And Ready To Assist You😘",
 };
