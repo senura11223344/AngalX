f390979ac103ecb8dd4c0812a25e377b494444d8
senura11223344 <senuradarshitha0905@gmail.com>
2025-06-22 18:48:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "VmNzSTrJ#fcqugUfGN1P9iyFkxYITC2dP29bDWUvN6GUG36CbEBQ",
+SESSION_ID: process.env.SESSION_ID || "N691xaRK#Dz7Z9DFUW1WNboRnjrG5DxOA4dO6vxKruy_nchmdnRQ",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Thinura-Nethz/HELP/refs/heads/main/ChatGPT%20Image%20Jun%2015%2C%202025%2C%2004_55_04%20PM.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*👀Hey, I'm Angal-X Multi-Deveice (MD⚙) Whatsapp Bot..* \n\n *Now I'm Alive And Ready To Assist You😘💕* \n\n *🙄Is Any Problem Plz Msg Me +94 77 457 1418* \n\n *You Can Join My Whatsapp Group Also👇* \n\n https://chat.whatsapp.com/DgkRi449IO565xc2UZGcvA \n\n *Developer-Thinura_Nethz*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",    
